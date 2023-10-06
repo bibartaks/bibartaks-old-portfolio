5a4d4a37d5484b54f26c7cad0890973c3b7c746f
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-10-06 16:07:19 +0600

    Added about section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 new fullpage("#fullpage", {
   licenseKey: "D3E291AE-D65447D5-84602E0F-3F7962D0",
-  sectionsColor: ["#121212"],
+  sectionsColor: ["#121212", "#121212"],
   navigation: true,
   navigationPosition: "right",
   responsiveWidth: 800,
