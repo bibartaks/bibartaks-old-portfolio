a7920e1ca8d0af9794bbb2c46769499204e5f8b3
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-10-06 23:56:19 +0600

    Added contact section make all the thing responsive

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 new fullpage("#fullpage", {
   licenseKey: "D3E291AE-D65447D5-84602E0F-3F7962D0",
-  sectionsColor: ["#121212", "#121212", "#121212", "#121212"],
+  sectionsColor: ["#121212", "#121212", "#121212", "#121212", "#121212"],
   navigation: true,
   navigationPosition: "right",
   responsiveWidth: 800,
+  navigationTooltips: [
+    "Intro",
+    "About Me",
+    "What I use",
+    "What have I build",
+    "Contact me",
+  ],
+  anchors: [
+    "landing",
+    "about-me",
+    "what-i-use",
+    "what-have-i-built",
+    "contact",
+  ],
 })
 
 document.addEventListener("DOMContentLoaded", () => {
